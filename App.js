1d577b924eb47c460409d2d0b9c35fda027d8ab2
Abdullah-Ahmed1 <abdullah.ahmed10001@gmail.com>
2021-12-27 13:23:47 +0500

    second commit

--- App.js
+++ App.js
@@ -1,20 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  Button,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
-export default function App() {
+function WorldStatisticsScreen({ navigation }) {
+  const [confirmedCases, setConfirmedCases] = useState();
+  const [worldPopulation, setWorldPopulation] = useState();
+  React.useEffect(() => {
+    fetch("https://covid-19-data.p.rapidapi.com/totals", {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+        "x-rapidapi-key": "7333042294mshccf262fd41bdb78p1daf5ajsn155b40f9549c",
+      },
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        setConfirmedCases(res[0].confirmed);
+        console.log("data", res[0].confirmed);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    //--------------------------------------------------------------------
+    fetch("https://world-population.p.rapidapi.com/worldpopulation", {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "world-population.p.rapidapi.com",
+        "x-rapidapi-key": "7333042294mshccf262fd41bdb78p1daf5ajsn155b40f9549c",
+      },
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log("world population", responseJson);
+        setWorldPopulation(responseJson.body.world_population);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Total Confirmed Cases: {confirmedCases}</Text>
+      <Text>Total world population is : {worldPopulation}</Text>
+    </View>
+  );
+}
+function CountryStaticsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>this is country statistics screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function FavoriteCountryScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+const Drawer = createDrawerNavigator();
+
+function App({ navigation }) {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="World Statistics Screen">
+        <Drawer.Screen
+          name="World Statistics"
+          component={WorldStatisticsScreen}
+        />
+        <Drawer.Screen
+          name="Country statistics"
+          component={CountryStaticsScreen}
+        />
+        <Drawer.Screen
+          name="Favorite Countries"
+          component={FavoriteCountryScreen}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
